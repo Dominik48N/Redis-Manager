2663959cef40086ca6d34ab49d444ca746c0fdb9
Dominik <admin@dominik48n.de>
2021-05-05 20:36:12 +0200

    Add a DefaultRedisProvider with default methods

--- src/main/kotlin/de/dominik48n/redis/manager/DefaultRedisProvider.kt
+++ src/main/kotlin/de/dominik48n/redis/manager/DefaultRedisProvider.kt
@@ -8,6 +8,10 @@ class DefaultRedisProvider : RedisProvider {
 
     override fun start(redisDatabase: RedisDatabase) {
         this.initClasses(redisDatabase)
+
+        this.jedis.auth(redisDatabase.password)
+        this.jedis.select(redisDatabase.databaseIndex)
+        this.jedis.connect()
     }
 
     override fun stop() {
--- src/main/kotlin/de/dominik48n/redis/manager/RedisDatabase.kt
+++ src/main/kotlin/de/dominik48n/redis/manager/RedisDatabase.kt
@@ -2,4 +2,6 @@ package de.dominik48n.redis.manager
 
 data class RedisDatabase(
     val hostName: String,
-    val port: Int)
+    val port: Int,
+    val password: String,
+    val databaseIndex: Int)
