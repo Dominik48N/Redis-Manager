d8a2fa8446a03db2276553080ab6387a3d8cf095
Dominik <admin@dominik48n.de>
2021-05-29 15:40:15 +0200

    Add method comments

--- src/main/kotlin/de/dominik48n/redis/manager/RedisManager.kt
+++ src/main/kotlin/de/dominik48n/redis/manager/RedisManager.kt
@@ -2,20 +2,34 @@ package de.dominik48n.redis.manager
 
 import redis.clients.jedis.Jedis
 
-class RedisManager(val jedis: Jedis) {
+class RedisManager(private val jedis: Jedis) {
 
+    /**
+     * Inserts a new entry in redis
+     */
     fun append(key: String, value: Any): Long {
         return this.jedis.append(key, value.toString())
     }
 
+    /**
+     * Removes an existing entry
+     */
     fun remove(key: String): Long {
         return this.jedis.del(key)
     }
 
+    /**
+     * Checks whether an entry exists
+     *
+     * @return true, when an value was founded
+     */
     fun exists(key: String): Boolean {
         return this.jedis.exists(key)
     }
 
+    /**
+     * Removes an entry after the specified number of seconds
+     */
     fun expire(key: String, seconds: Long): Long {
         return this.jedis.expire(key, seconds)
     }
--- src/main/kotlin/de/dominik48n/redis/manager/RedisProvider.kt
+++ src/main/kotlin/de/dominik48n/redis/manager/RedisProvider.kt
@@ -2,10 +2,19 @@ package de.dominik48n.redis.manager
 
 interface RedisProvider {
 
+    /**
+     * @return RedisManager
+     */
     fun getManager(): RedisManager
 
+    /**
+     * Starts the redis provider
+     */
     fun start(redisDatabase: RedisDatabase)
 
+    /**
+     * Stops the redis provider
+     */
     fun stop()
 
 }
